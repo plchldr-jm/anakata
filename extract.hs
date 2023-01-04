e91065dd75d5d8f3520da9d17cc6b75436b39ef2
Jim <79030805+plchldr-jm@users.noreply.github.com>
2023-01-04 15:39:07 -0500

    .

---
+++ extract.hs
@@ -0,0 +1,23 @@
+import Data.List (isPrefixOf)
+
+extract :: String -> String
+
+extract "" = ""
+
+extract cartridge 
+  | "<script>\n" 
+    `isPrefixOf` cartridge
+  = let 
+    code crt
+      | "\n</script>\n" 
+        `isPrefixOf` crt
+      = ""
+      | otherwise
+      = case crt of 
+        (c:cs) -> 
+          (c:code cs)
+  in code . drop 9 $ cartridge
+  | otherwise
+  = extract . tail $ cs
+
+--extractPL :: String -> String -> String
